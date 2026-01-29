a1adef3a37fd30518635a9cfc44721266528be3a
Alin <alin.cristian977@gmail.com>
2026-01-29 17:49:20 +0100

    first commit

---
+++ FrenchAdverbes/Constants.cs
@@ -0,0 +1,56 @@
+﻿namespace FrenchAdverbes;
+
+public class Constants
+{
+
+    // A
+    public const string Absolument = "absolument";
+    public const string Ailleurs = "ailleurs";
+    public const string AujourdHui = "aujourd'hui";
+    public const string Aussi = "aussi";
+    public const string Autour = "autour";
+    public const string Avant = "avant";
+    public const string AvantHier = "avant-hier";
+    public const string Apres = "après";
+
+    // B
+    public const string Beaucoup = "beaucoup";
+    public const string Bien = "bien";
+
+    // C
+    public const string Combien = "combien";
+
+    // D
+    public const string Dedans = "dedans";
+    public const string Dehors = "dehors";
+    public const string Devant = "devant";
+
+    // J
+    public const string Juste = "juste";
+
+    // M
+    public const string Malheureusement = "malheureusement";
+    public const string Maintenant = "maintenant";
+
+    // N
+
+    // P
+    public const string Pres = "près";
+    public const string Parfois = "parfois";
+    public const string Partout = "partout";
+    public const string Peu = "peu";
+    public const string Plus = "plus";
+    public const string Pourquoi = "pourquoi";
+    public const string Puis = "puis";
+    public const string Parfaitement = "parfaitement";
+    public const string PeutEtre = "peut-être";
+
+    // S
+    public const string Souvent = "souvent";
+
+    // T
+    public const string Tres = "très";
+
+    // etc..
+
+}
