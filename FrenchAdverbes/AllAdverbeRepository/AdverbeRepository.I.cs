7a94d649b3a32bccb2a99c3e1dfb8e0961cb6cce
Alin <alin.cristian977@gmail.com>
2026-04-30 23:08:24 +0300

    i

--- FrenchAdverbes/AllAdverbeRepository/AdverbeRepository.I.cs
+++ FrenchAdverbes/AllAdverbeRepository/AdverbeRepository.I.cs
@@ -7,5 +7,10 @@ internal static partial class AdverbeRepository
     public static readonly IReadOnlyList<string> I = new[]
     {
         Constants.Immediatement,
+        Constants.Incroyablement,
+        Constants.Instinctivement,
+        Constants.Intensement,
+        Constants.Involontairement,
+        Constants.Inutilement,
     };
 }
--- FrenchAdverbes/AllConstants/Constants.I.cs
+++ FrenchAdverbes/AllConstants/Constants.I.cs
@@ -3,4 +3,9 @@ namespace FrenchAdverbes.AllConstants;
 public partial class Constants
 {
     public const string Immediatement = "immédiatement";
+    public const string Incroyablement = "incroyablement";
+    public const string Instinctivement = "instinctivement";
+    public const string Intensement = "intensément";
+    public const string Involontairement = "involontairement";
+    public const string Inutilement = "inutilement";
 }
