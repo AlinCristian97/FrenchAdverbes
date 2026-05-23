46dec7ea028941285f034fbded9d3ed0476813ee
Alin <alin.cristian977@gmail.com>
2026-05-24 02:47:58 +0300

    r

--- FrenchAdverbes/AllAdverbeRepository/AdverbeRepository.R.cs
+++ FrenchAdverbes/AllAdverbeRepository/AdverbeRepository.R.cs
@@ -7,5 +7,10 @@ internal static partial class AdverbeRepository
     public static readonly IReadOnlyList<string> R = new[]
     {
         Constants.Rapidement,
+        Constants.Regulierement,
+        Constants.Reellement,
+        Constants.Remarquablement,
+        Constants.Resolument,
+        Constants.Recemment,
     };
 }
--- FrenchAdverbes/AllConstants/Constants.R.cs
+++ FrenchAdverbes/AllConstants/Constants.R.cs
@@ -3,4 +3,9 @@ namespace FrenchAdverbes.AllConstants;
 public partial class Constants
 {
     public const string Rapidement = "rapidement";
+    public const string Regulierement = "régulièrement";
+    public const string Reellement = "réellement";
+    public const string Remarquablement = "remarquablement";
+    public const string Resolument = "résolument";
+    public const string Recemment = "récemment";
 }
