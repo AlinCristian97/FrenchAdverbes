b7d8af7896d8eface871285c525af45a9ac07d09
Alin <alin.cristian977@gmail.com>
2026-05-24 02:46:18 +0300

    q

--- FrenchAdverbes/AllAdverbeRepository/AdverbeRepository.Q.cs
+++ FrenchAdverbes/AllAdverbeRepository/AdverbeRepository.Q.cs
@@ -7,5 +7,7 @@ internal static partial class AdverbeRepository
     public static readonly IReadOnlyList<string> Q = new[]
     {
         Constants.Quand,
+        Constants.Quotidiennement,
+        Constants.Quasiment,
     };
 }
--- FrenchAdverbes/AllConstants/Constants.Q.cs
+++ FrenchAdverbes/AllConstants/Constants.Q.cs
@@ -3,4 +3,7 @@ namespace FrenchAdverbes.AllConstants;
 public partial class Constants
 {
     public const string Quand = "quand";
+    public const string Quotidiennement = "quotidiennement";
+    public const string Quasiment = "quasiment";
+}
 }
