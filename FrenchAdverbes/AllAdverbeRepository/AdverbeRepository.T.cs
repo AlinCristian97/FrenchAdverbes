897b0213f35df90fb6821b854d87ce4a77d2f93b
Alin <alin.cristian977@gmail.com>
2026-05-24 02:51:15 +0300

    t

--- FrenchAdverbes/AllAdverbeRepository/AdverbeRepository.T.cs
+++ FrenchAdverbes/AllAdverbeRepository/AdverbeRepository.T.cs
@@ -11,5 +11,10 @@ internal static partial class AdverbeRepository
         Constants.Tranquillement,
         Constants.Tard,
         Constants.Tant,
+        Constants.Timidement,
+        Constants.Tendrement,
+        Constants.Terriblement,
+        Constants.Typiquement,
+        Constants.Temporairement,
     };
 }
--- FrenchAdverbes/AllConstants/Constants.T.cs
+++ FrenchAdverbes/AllConstants/Constants.T.cs
@@ -7,4 +7,9 @@ public partial class Constants
     public const string Tranquillement = "tranquillement";
     public const string Tard = "tard";
     public const string Tant = "tant";
+    public const string Timidement = "timidement";
+    public const string Tendrement = "tendrement";
+    public const string Terriblement = "terriblement";
+    public const string Typiquement = "typiquement";
+    public const string Temporairement = "temporairement";
 }
